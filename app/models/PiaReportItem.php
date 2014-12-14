b60a24a274e677d86decf41bc45215f69df18cea
hsine <extraordinary1022@gmail.com>
2014-12-14 16:54:05 +0800

    PiaReport & PiaReportItem model modify dept> code&email migration file Apply migration dept> code sql update

---
+++ app/models/PiaReportItem.php
@@ -0,0 +1,37 @@
+<?php
+
+class PiaReportItem extends PiaBase {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'report_item';
+	protected $primaryKey = 'ri_id';
+	
+	public function save(array $options = array()){
+
+		$validator = Validator::make
+		(
+		    array(
+	    		"ri_id" => $this->ri_id,
+	    		"r_id" => $this->r_id,
+	    		"ri_base" => $this->ri_base,
+	    		"ri_discover" => $this->ri_discover,
+	    		"ri_recommand" => $this->ri_recommand,
+		    	),
+		    array(
+		        "ri_id" => 'required',
+		        "r_id" => 'required',
+		        "ri_base" => 'required',
+		        "ri_discover" => 'required',
+		        "ri_recommand" => 'required',
+		    )
+		);
+		if($validator->fails())
+			throw new Exception($validator->messages());
+		parent::save($options);
+	}
+
+}
