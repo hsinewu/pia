971d545f125483d9ceeb8dd4752c8807cd0b98e6
PastLeo <chgu82837@gmail.com>
2015-01-04 20:10:52 +0800

    change menu

--- app/views/menu.blade.php
+++ app/views/menu.blade.php
@@ -5,7 +5,7 @@
   <a class="list-group-item" href="{{ route('admin_info','dept') }}">單位資料表</a>
   <a class="list-group-item" href="{{ route('admin_info','person') }}">人員資料表</a>
   <a class="list-group-item" href="{{ route('admin_info','event') }}">事件設定</a>
-  <a class="list-group-item" href="{{ route('admin_info','audit') }}">稽核設定</a>
+  <a class="list-group-item" href="{{ route('admin_info','audit') }}">稽核任務指派</a>
   <a class="list-group-item" href="{{ route('admin_cal') }}">行事曆</a>
   <a class="list-group-item" href="{{ route('admin_reports') }}">回報狀態</a>
 </div>
