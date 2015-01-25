8f5e18c24da812b952bbf4a1347836c49d8e355c
hsine <extraordinary1022@gmail.com>
2015-01-25 19:47:35 +0800

    feedback form simple

--- app/views/auditee/feedback.blade.php
+++ app/views/auditee/feedback.blade.php
@@ -33,15 +33,56 @@
       @include("menu")
     </div>
     <div class="col-xs-9">
+      
 
       <div class="panel panel-default">
         <!-- Default panel contents -->
         <div class="panel-heading" style="height: 55px; line-height: 34px;">
           {{ $title }}
         </div>
+          <form class="form-horizontal" role="form" action="" method="POST">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">問題或缺失說明</label>
+                  <div class="col-sm-10">
+                  <input type="text">
+                  </div>
+                </div>
 
-        <p>Contents</p>
-          <p>問題缺失: <input type="text" name="" id=""></p>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">原因分析</label>
+                  <div class="col-sm-10">
+                  <input type="text">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">問題或缺失說明</label>
+                  <div class="col-sm-10">
+                  <input type="text">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">矯正措施</label>
+                  <div class="col-sm-10">
+                  <input type="text">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">預防措施</label>
+                  <div class="col-sm-10">
+                  <input type="text">
+                  </div>
+                </div>
+                
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default">送出</button>
+              </div>
+            </div>
+          </form>
+
       </div>
     </div>
   </div>
