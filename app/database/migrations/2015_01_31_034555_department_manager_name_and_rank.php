964085afc6333b2027a181c33029b18a9b7ecedf
hsine <extraordinary1022@gmail.com>
2015-01-31 11:54:12 +0800

    migration: dept new manager name+rank field

---
+++ app/database/migrations/2015_01_31_034555_department_manager_name_and_rank.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DepartmentManagerNameAndRank extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('dept', function($table)
+		{
+		    $table->string('manager_name');
+		    $table->string('manager_rank')->default('主管');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('dept', function($table)
+		{
+		    $table->dropColumn('manager_name');
+		    $table->dropColumn('manager_rank');
+		});
+	}
+
+}
